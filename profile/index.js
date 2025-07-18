65af1da282e267d0a2f761c971774119f36d35a1
Shnupel <ufamail.com2@gmail.com>
2025-07-18 15:50:17 +0300

    change by swagger

--- profile/index.js
+++ profile/index.js
@@ -3,16 +3,16 @@ const { Router } = require('express');
 const router = Router();
 
 const user = {
-  "nameEng": "Fyodor Markin",
-  "nameRu": "Маркин Фёдор Сергеевич",
+  "name_eng": "Fyodor Markin",
+  "name_ru": "Маркин Фёдор Сергеевич",
   "alias": "@meld_i",
   "email": "f.markin@innopolis.university",
   "position": "Intern TA",
   "institute": "Институт разработки ПО и программной инженерии",
   "workload": 0.7,
-  "studentType": "MS1",
+  "student_type": "MS1",
   "degree": true,
-  "FSRO": "employnment",
+  "fsro": "employnment",
   "languages": [
     {
       "language": "Russian"
@@ -20,37 +20,47 @@ const user = {
   ],
   "courses": [
     {
-      "id": "courseInstance_id"
+      "id": "course_instance_id"
     }
   ],
-  "employnmentType": "Combination of positions",
-  "hiringStatus": "??",
+  "employnment_type": "Combination of positions",
+  "hiring_status": "??",
   "mode": "remote",
-  "maxLoad": 40,
-  "frontalHours": 40,
-  "extraActivities": 1.5,
-  "workloadStats": {
-    "uniteStat": [
-      {
-        "id": "T1",
-        "classes": {
-          "lec": 1,
-          "tut": 2,
-          "lab": 3,
-          "elec": 4,
-          "rate": 5
-        }
-      }
-    ],
+  "max_load": 40,
+  "frontal_hours": 40,
+  "extra_activities": 1.5,
+  "workload_stats": {
+    "t1": {
+      "lec_hours": 134,
+      "tut_hours": 123,
+      "lab_hours": 123,
+      "elective_hours": 123,
+      "rate": 123
+    },
+    "t2": {
+      "lec_hours": 134,
+      "tut_hours": 123,
+      "lab_hours": 123,
+      "elective_hours": 123,
+      "rate": 123
+    },
+    "t3": {
+      "lec_hours": 134,
+      "tut_hours": 123,
+      "lab_hours": 123,
+      "elective_hours": 123,
+      "rate": 123
+    },
     "total": {
-      "totalLec": 1,
-      "totalTut": 2,
-      "totalLab": 3,
-      "totalElec": 12,
-      "totalRate": 12
+      "lec_hours": 134,
+      "tut_hours": 123,
+      "lab_hours": 123,
+      "elective_hours": 123,
+      "rate": 123
     }
   }
 }
+
 
 router.get('/getUser/:id', (req, res) => {
   const id = req.params.id;
@@ -58,13 +68,13 @@ router.get('/getUser/:id', (req, res) => {
   return res.json(user)
 });
 
-router.get('/getAllUsers/', (req, res) => {
+router.get('/getAllProfiles/', (req, res) => {
   res.json({
-    data: new Array(10).fill(user).map((user, index) => ({ ...user, id: index })),
+    profiles: new Array(10).fill(user).map((user, index) => ({ ...user, profile_id: index })),
   })
 })
 
-router.post('/addUser/', (req, res) => {
+router.post('/addProfile/', (req, res) => {
   const user = req.body;
   if (!user) { 
      return res.status(502).json({ message: 'cant create user' });
@@ -80,22 +90,19 @@ router.get("/filters", (req, res) => {
     "filters": {
       "institute": [
         {
-          "Робототехника": 23
-        },
-        {
-          "МЛ": 34
+          "id": 23,
+          "name": "institute_filter_name"
         }
       ],
       "position": [
         {
-          "TA": 23
-        },
-        {
-          "Primary Instructor": 34
+          "id": 23,
+          "name": "position_filter_name"
         }
       ]
     }
-  })
+  }
+  )
 })
 
 module.exports = router;
