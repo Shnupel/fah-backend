91a6feccce3b14d8d18b2ded1242c05cf00f92db
shnupel <ufamail.com2@gmail.com>
2025-07-21 05:21:23 +0300

    changes

--- profile/index.js
+++ profile/index.js
@@ -3,66 +3,81 @@ const { Router } = require('express');
 const router = Router();
 
 const user = {
-  "name_eng": "Fyodor Markin",
-  "name_ru": "Маркин Фёдор Сергеевич",
-  "alias": "@meld_i",
-  "email": "f.markin@innopolis.university",
-  "position": "Intern TA",
-  "institute": "Институт разработки ПО и программной инженерии",
-  "workload": 0.7,
-  "student_type": "MS1",
+  "year": 0,
+  "name_eng": "string",
+  "name_ru": "string",
+  "alias": "string",
+  "email": "string",
+  "position_name": "string",
+  "institute_names": [
+    "string"
+  ],
+  "student_type": "string",
   "degree": true,
-  "fsro": "employnment",
+  "fsro": "string",
   "languages": [
     {
-      "language": "Russian"
+      "language_code": "string"
     }
   ],
-  "courses": [
-    {
-      "id": "course_instance_id"
-    }
-  ],
-  "employnment_type": "Combination of positions",
-  "hiring_status": "??",
-  "mode": "remote",
-  "max_load": 40,
-  "frontal_hours": 40,
-  "extra_activities": 1.5,
+  "employnment_type": "string",
+  "hiring_status": "string",
+  "mode": "string",
+  "max_load": 0,
+  "frontal_hours": 0,
+  "extra_activities": 0,
   "workload_stats": {
     "t1": {
-      "lec_hours": 134,
-      "tut_hours": 123,
-      "lab_hours": 123,
-      "elective_hours": 123,
-      "rate": 123
+      "lec_hours": 0,
+      "tut_hours": 0,
+      "lab_hours": 0,
+      "elective_hours": 0,
+      "rate": 0
     },
     "t2": {
-      "lec_hours": 134,
-      "tut_hours": 123,
-      "lab_hours": 123,
-      "elective_hours": 123,
-      "rate": 123
+      "lec_hours": 0,
+      "tut_hours": 0,
+      "lab_hours": 0,
+      "elective_hours": 0,
+      "rate": 0
     },
     "t3": {
-      "lec_hours": 134,
-      "tut_hours": 123,
-      "lab_hours": 123,
-      "elective_hours": 123,
-      "rate": 123
+      "lec_hours": 0,
+      "tut_hours": 0,
+      "lab_hours": 0,
+      "elective_hours": 0,
+      "rate": 0
     },
     "total": {
-      "lec_hours": 134,
-      "tut_hours": 123,
-      "lab_hours": 123,
-      "elective_hours": 123,
-      "rate": 123
+      "lec_hours": 0,
+      "tut_hours": 0,
+      "lab_hours": 0,
+      "elective_hours": 0,
+      "rate": 0
     }
   }
 }
 
+const simpleUser = {
+  profile_id: 0,
+  name_eng: "string",
+  alias: "string",
+  email: "string",
+  position_name: "string",
+  institute_names: [
+    "string"
+  ]
+}
 
-router.get('/getUser/:id', (req, res) => {
+const simpleUsers = {
+  profiles: new Array(10).fill(simpleUser).map((user, i) => ({ ...user, profile_id: i }))
+}
+
+const users = {
+  profiles: new Array(10).fill(user).map((user, index) => ({ ...user, profile_id: index })),
+}
+
+router.get('/getProfile/:id', (req, res) => {
   const id = req.params.id;
   if (!id) return res.status(404).send('Not Found');
   return res.json(user)
@@ -70,9 +85,7 @@ router.get('/getUser/:id', (req, res) => {
 
 
 router.get('/getAllProfiles/', (req, res) => {
-  res.json({
-    profiles: new Array(10).fill(user).map((user, index) => ({ ...user, profile_id: index })),
-  })
+  res.json(simpleUsers);
 })
 
 router.post('/addProfile/', (req, res) => {
@@ -81,29 +94,19 @@ router.post('/addProfile/', (req, res) => {
      return res.status(502).json({ message: 'cant create user' });
   }
   console.log(user);
-  return res.json({
-    message: 'User added successfully',
-  })
-});
 
-router.get("/filters", (req, res) => {
-  res.status(200).json({
-    "filters": {
-      "institute": [
-        {
-          "id": 23,
-          "name": "institute_filter_name"
-        }
-      ],
-      "position": [
-        {
-          "id": 23,
-          "name": "position_filter_name"
-        }
-      ]
-    }
+  const _user = {
+    profile_id: 123,
+    name_eng: user.name_eng,
+    email: user.email,
+    alias: user.alias,
+    institute_names: ["ist1", "inst2"],
+    position_name: "positions name here"
   }
-  )
-})
+
+  simpleUsers.profiles = [_user, ...simpleUsers.profiles];
+
+  return res.json(_user)
+});
 
 module.exports = router;
